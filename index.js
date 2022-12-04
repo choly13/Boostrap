ffe6cce78369f6a89f98dc13030e7623d36b48a7
choly13 <avellanedaerika3@gmail.com>
2022-12-04 19:14:04 -0300

    imagenes

---
+++ index.js
@@ -0,0 +1,9 @@
+let a = 6
+if (a < 5){
+    if (a === 1){
+    console.log("El numero es menor a 5 y es 1");
+    } else {
+        console.log("El numero es menor a 5, pero no es 1")
+    }else{
+        console.log("El numero es mayor a 5")
+    }
