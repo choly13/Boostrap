c09c96db8c6ef5c25a5f927ce99c7e61a353ebdd
choly13 <avellanedaerika3@gmail.com>
2022-12-09 20:20:08 -0300

    javascript

---
+++ coman.js
@@ -0,0 +1,6 @@
+var resultado = window.confirm('Bienvenido a Historia del Arte');
+if (resultado === true) {
+    window.alert('Gracias por elegirnos');
+} else { 
+    window.alert('Sigamos leyendo');
+}
